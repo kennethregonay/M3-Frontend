7b4f554aea298c802f58eb2986a692ecf8a9ea12
Netkee <kenneth.regonay@gmail.com>
2021-01-24 18:00:34 +0800

    added the get fetch for retriving records and Inserting records.

--- javascript/employee.js
+++ javascript/employee.js
@@ -1,25 +1,69 @@
-const postlist = document.querySelector('post-list');
 const api = 'http://m2-act.test/api/employee';
 let output = '';
+const add = document.getElementById('form-input');
+const fname = document.getElementById('fname');
+const mname = document.getElementById('mname');
+const lname = document.getElementById('lname');
+const address = document.getElementById('address');
+const gender = document.getElementById('gender');
+const bdate = document.getElementById('bdate');
+const poscode = document.getElementById('poscode');
 
-//Getting all the data from the api.
+const  getdata = (emp) => {
+    emp.forEach(employee => {
+        output 
+        += `<tr>
+            <td>${employee.Firstname}</td>
+            <td>${employee.MiddleName}</td>
+            <td>${employee.Lastname}</td>
+            <td>${employee.Address}</td>
+            <td>${employee.Gender}</td>
+            <td>${employee.Birthdate}</td>
+            <td>${employee.Position_Code}</td>
+            <td>
+              <a href="update.php?update=" class="btn btn-info">Update </a>\
+               <a href="employee.php?delete=" class="btn btn-danger">Delete</a>\
+            </td>
+          </tr>`;
+          });
+            document.getElementById('table-body').innerHTML = output; 
+
+}
+
 fetch(api)
 .then((res) => res.json())
-.then((data => {
- let emp = data['data'];
- emp.forEach(employee => {
-    output += '<div class="card col-md-4 bg-light" > <div class="card-body"> \ 
-     <h5 class="card-title">Card title</h5>\
-     <h6 class="card-subtitle mb-2 text-muted">\
-     Card subtitle</h6><p class="card-text">\
-     Some quick example text to build on the card title and make up the bulk of the cards content.</p>\
-     <a href="#" class="card-link">Update</a>\
-     <a href="#" class="card-link">\
-     Delete</a></div></div>';
-   });
-   postlist.innerHTML = output;
-}))
+.then(data => getdata(data['data']))  
 .catch((error) => {
     console.log(error);
 })
+
+// Add a new Record
+add.addEventListener('submit',(e) => {
+    e.preventDefault();
+    fetch (api,{
+        method: 'POST',
+        Headers:{
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+        },
+        body:JSON.stringify({
+            Firstname  : fname.value,
+            Lastname   : lname.value,
+            MiddleName : mname.value,
+            Address : address.value,
+            Gender : gender.value,
+            Birthdate : bdate.value,
+        })
+    })
+    .then (res => res.json())
+    .then ((data => {
+        const arr = [];
+        arr.push(data);
+        getdata(arr);
+        console.log ('Record Successfully Added.');
+    }))
+    
+})
+
+
 
