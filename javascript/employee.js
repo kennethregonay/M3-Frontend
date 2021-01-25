79e108f0b8ac1594fc80688f157fd832a95be29a
Netkee <kenneth.regonay@gmail.com>
2021-01-25 13:24:39 +0800

    added a fetch delete/ and Create on the API.

--- javascript/employee.js
+++ javascript/employee.js
@@ -9,10 +9,13 @@ const gender = document.getElementById('gender');
 const bdate = document.getElementById('bdate');
 const poscode = document.getElementById('poscode');
 
+
+// Retriving Records in the Employee Table.
 const  getdata = (emp) => {
     emp.forEach(employee => {
         output 
         += `<tr>
+            <td>${employee.id}</td>
             <td>${employee.Firstname}</td>
             <td>${employee.MiddleName}</td>
             <td>${employee.Lastname}</td>
@@ -21,13 +24,12 @@ const  getdata = (emp) => {
             <td>${employee.Birthdate}</td>
             <td>${employee.Position_Code}</td>
             <td>
-              <a href="update.php?update=" class="btn btn-info">Update </a>\
-               <a href="employee.php?delete=" class="btn btn-danger">Delete</a>\
+              <a href="index.html?update=${employee.id}" class="btn btn-info" id = "update" onclick="update(${employee.id})">Update </a>
+               <a href="employee.html?delete=${employee.id}" class="btn btn-danger" id "delete" onclick="del(${employee.id})">Delete</a>
             </td>
           </tr>`;
           });
-            document.getElementById('table-body').innerHTML = output; 
-
+            document.getElementById('table-body').innerHTML = output;
 }
 
 fetch(api)
@@ -37,33 +39,55 @@ fetch(api)
     console.log(error);
 })
 
-// Add a new Record
+// Add a new Record in the Employee Table
 add.addEventListener('submit',(e) => {
     e.preventDefault();
     fetch (api,{
         method: 'POST',
-        Headers:{
-            'Accept': 'application/json',
-            'Content-Type': 'application/json'
+        headers:{
+            'Content-Type': 'application/json',
+            'Accept' : 'application/json',
         },
         body:JSON.stringify({
             Firstname  : fname.value,
+            Middlename : mname.value,
             Lastname   : lname.value,
-            MiddleName : mname.value,
             Address : address.value,
             Gender : gender.value,
             Birthdate : bdate.value,
-        })
+            Position_Code : poscode.value,
+        }),
+    })
+    .then ((res) => console.log(res.json()))
+    .then (data =>{
+        getdata(data);
+        location.reload();
+    })
+    .catch((error) => {
+        console.log(error);
+    })
+})
+
+
+//delete the data in the DB using Fetch 
+function del (id) {
+    fetch (`${api}/${id}`,{
+        method: 'DELETE',
+        headers: {
+            'Content-Type': 'application/json',
+            'Accept' : 'application/json',
+        } 
     })
     .then (res => res.json())
-    .then ((data => {
-        const arr = [];
-        arr.push(data);
-        getdata(arr);
-        console.log ('Record Successfully Added.');
-    }))
+    .then(() => location.reload())
+    .catch((error) => {
+         console.log (error);
+    })
+}
+// the content in the Employee Form
+function update (id) {
     
-})
+}
 
 
 
